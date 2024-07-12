ea93dfef8aa51b23dd0d9a692f471ba0e4e3b6f9
Dima K <@users.noreply.github.com>
2024-07-12 10:18:46 +0200

    solved the shcool challenge

--- school.py
+++ school.py
@@ -1,8 +1,22 @@
 # pylint:disable=C0111,C0103
 
+import sqlite3
+
+
+
 def students_from_city(db, city):
     """return a list of students from a specific city"""
-    pass  # YOUR CODE HERE
+    query = f"""
+    SELECT *
+    FROM students s
+    WHERE birth_city = '{city}'
+    """
+    conn = sqlite3.connect('data/school.sqlite')
+    db = conn.cursor()
+    db.execute(query)
+    results = db.fetchall()
+    return results
+
 
 
 # To test your code, you can **run it** before running `make`
